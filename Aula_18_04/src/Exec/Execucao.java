a60376ccf1b5d3809f9fd37be4a08f6b9a0d1924
unknown <eziopneto@globomail.com>
2019-04-19 12:42:51 -0300

    visao

---
+++ Aula_18_04/src/Exec/Execucao.java
@@ -0,0 +1,13 @@
+package Exec;
+
+import aula.Pessoa;
+
+public class Execucao {
+	
+		public static void main (String [] args) {
+			// TODO Auti-genereted method stub
+			Pessoa ps = new Pessoa ();
+			ps.andar();
+		}
+
+}
